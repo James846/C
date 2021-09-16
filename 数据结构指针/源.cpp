13a4ab200aeb438f2b8b187bd8b9e86c609bcba5
James846 <2236328881@qq.com>
2021-09-16 21:13:38 +0800

    单链表

--- 数据结构指针/源.cpp
+++ 数据结构指针/源.cpp
@@ -1,12 +1,32 @@
 #include<iostream>
-typedef int ElemType;
-typedef struct Node
-{
+#include<stdio.h>
+
+typedef struct Node {
 	int data;
 	struct Node* next;
-}LNode,LinkList;
-int Get(LinkList* first, int i, ElemType* e)
-{
-	if (i < 1)return 0;
-	p = (*first).next;
+}Node;
+void createLink(Node* head, int size) {
+	Node* rear = head;
+	int i;
+	for (i = 0; i < size; i++) {
+		Node* newnode = (Node*)malloc(sizeof(Node));
+		newnode->next = NULL;
+		scanf_s("%d", &newnode->data);
+		rear->next = newnode;
+		rear = newnode;
+	}
+}
+void travelLink(Node* head) {
+	Node* p = head->next;
+	while (p != NULL) {
+		printf("%d\t", p->data);
+		p = p->next;
+	}
+	putchar('/n');
+}
+int main() {
+	Node* head = (Node*)malloc(sizeof(Node));
+	head->next = NULL;
+	createLink(head, 10);
+	travelLink(head);
 }
