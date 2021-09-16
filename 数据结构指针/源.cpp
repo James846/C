a263154f66d288d4a8b011f3ed9403769c20069a
James846 <2236328881@qq.com>
2021-09-16 11:38:26 +0800

    数据结构

--- 数据结构指针/源.cpp
+++ 数据结构指针/源.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
+typedef int ElemType;
 typedef struct Node
 {
 	int data;
 	struct Node* next;
-}LNode;
+}LNode,LinkList;
+int Get(LinkList* first, int i, ElemType* e)
+{
+	if (i < 1)return 0;
+	p = (*first).next;
+}
